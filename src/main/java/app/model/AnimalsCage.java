f00c2afed57789bc58aacbf57a27709bb4a2c003
deus <deus2001@mail.ru>
2026-02-18 21:10:30 +0700

    pogoCat

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,19 +1,28 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
     @Autowired
+    @Qualifier("dog")  // Явно указываем, что хотим бин с именем "dog"
     private Animal animal;
+
+    @Autowired
+    private Timer timer;  // Внедряем бин Timer
 
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());
         System.out.println("________________________");
+    }
+
+    public Timer getTimer() {
+        return timer;
     }
 }
--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,5 +1,8 @@
 package app.model;
 
+import org.springframework.stereotype.Component;
+
+@Component  // Добавляем эту аннотацию
 public class Timer {
 
     private Long nanoTime = System.nanoTime();
